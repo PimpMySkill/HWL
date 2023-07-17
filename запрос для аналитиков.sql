4be7831b678aff6baf8b4660a6af0b341a8ac904
Vladislav Ibragimov <Ibragimov.Vladislav@rambler.ru>
2023-07-17 20:54:24 +0300

    My first project!

---
+++ запрос для аналитиков.sql
@@ -0,0 +1,15 @@
+select *,
+i.brief as 'ФО', 
+case when max(d.indatetime) is null
+then 0
+else max(d.indatetime) as 'Дата последней операции'
+end
+min(d.indatetime) as 'Дата первой операции'
+case when count(d.dealid) is null
+then 'Отсутствуют операции'
+else cast (count(d.dealid) as char(20)) as 'Количество сделок'
+end --count(d.dealid)
+from tinstrument i with (nolock)
+left join tdeal d with(nolock)
+on i.instrumentid = d.instrumentid
+group by i.brief
